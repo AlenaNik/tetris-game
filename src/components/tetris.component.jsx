b88bd6af54db556edbe867f7f63de1af5182dc29
AlenaNik <alena.nik.digital@gmail.com>
2019-08-17 19:31:04 +0200

    tetromino drawed

--- src/components/tetris.component.jsx
+++ src/components/tetris.component.jsx
@@ -1,4 +1,5 @@
 import React, {useState} from 'react';
+import {createStage} from "../gameHelpers";
 
 import {usePlayer} from "../hooks/usePlayer";
 import {useStage} from "../hooks/useStage";
@@ -12,13 +13,41 @@ const Tetris = () => {
     const [ dropTime, setDropTime] = useState(null);
     const [gameOver, setGameOver] = useState(false);
 
-    const [player] = usePlayer();
+    const [player, updatePlayerPos, resetPlayer] = usePlayer();
     const [stage, setStage] = useStage(player);
 
     console.log('re-render');
 
+    const movePlayer = dir => {
+        updatePlayerPos({x: dir, y: 0});
+    }
+    const startGame = () => {
+    // Reset everything
+        setStage(createStage());
+        resetPlayer();
+    }
+
+    const drop = () => {
+        updatePlayerPos({ x: 0, y: 1, collided: false})
+    }
+
+    const dropPlayer = () => {
+    drop();
+    }
+    const move = ({keyCode}) => {
+        if(!gameOver) {
+            if (keyCode === 37) {
+                movePlayer(-1);
+            } else if (keyCode === 39) {
+                movePlayer(1);
+            } else if (keyCode === 40) {
+                dropPlayer();
+            }
+        }
+    }
+
 return (
-    <StyledTetrisWrapper>
+    <StyledTetrisWrapper role="button" tabIndex="0" onKeyDown={e => move(e)}>
         <StyledTetris>
         <Stage stage={stage}/>
         <aside>
@@ -31,7 +60,7 @@ return (
                 <Display text="Level" />
             </div>
                 )}
-            <StartButton />
+            <StartButton onClick={startGame}/>
         </aside>
         </StyledTetris>
     </StyledTetrisWrapper>
